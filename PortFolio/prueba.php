433dc08bf2e1d5a0e2470803cbbddcec3031d2fa
ImDr3ek <ggdiego581@gmail.com>
2026-04-24 23:06:43 -0300

    que dor de cabeca kakk

---
+++ PortFolio/prueba.php
@@ -0,0 +1,35 @@
+<form method="post" action="showform.php">
+
+    
+    <fieldset>
+
+
+        <h2>Crie Sua Conta</h2>
+
+        <ul>
+            
+            <li>
+                
+            <label for="usuario">Nome de Usuario</label>
+            <input type="text" id="usuario" name="usuario" class="large"/>
+                   
+           </li>
+
+           <li>
+                 
+            <label for="senha">Crie Sua Senha</label>
+            <input type="text" id="senha" name="semha" class="large"/>
+
+           </li>
+            
+        </ul>
+
+        <input type="submit"
+        class="cria_seu_usuario"
+        value="Cria sua Conta">
+
+
+    </fieldset>
+
+
+</form>
