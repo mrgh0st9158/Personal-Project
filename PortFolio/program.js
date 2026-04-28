33e59651b8c3b984edcf1e6d0a9455ea1f598159
DIEGO CUBIELLES GUIJARRO <diego_guijarro@estudante.sesisenai.org.br>
2026-04-27 22:16:13 -0300

    DEU CERTO PORRAAAAA

--- PortFolio/program.js
+++ PortFolio/program.js
@@ -3,6 +3,17 @@ let newsenha, newusuario, usuario, senha
 function ciarconta(){
     newusuario = prompt("Crie seu usuario")
 newsenha = prompt("Crie sua senha")
+
+}
+function acessarconta(){
+    usuario = prompt("Usuario")
+    senha = prompt("Senha")
+if(usuario != newusuario || senha != newsenha){
+    alert("Errada a senha ou usuario")
+}
+else{
+    window.location.href = "file:///C:/Repos%20T3eK/Personal-Project/PortFolio/Index.html"
+}
 }
 
 
---
+++ PortFolio/scriptacesso.js
@@ -0,0 +1,6 @@
+let  usuario, senha
+
+function acessarconta(){
+    usuario = prompt("Nome de Usuario")
+    senha =
+}
