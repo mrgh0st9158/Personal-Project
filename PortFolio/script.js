fa6c949a7d64ef2e5c18b4b2122e8d3886e6a1ec
ImDr3ek <ggdiego581@gmail.com>
2026-05-02 03:59:32 -0300

    botoes funcionais e mudança no visual da pagina

--- PortFolio/script.js
+++ PortFolio/script.js
@@ -1,12 +1,37 @@
-function abrirBandas(){
-   
-}
-function abrirColaboradores(){
 
+//alert("Benvindo a BOW")
+
+function esconderTodo(){
+  const tela = document.querySelectorAll(".tela");
+  tela.forEach(function(tela){
+    tela.classList.remove("ativa")});
+ 
+}
+
+function abrirInicio(){
+esconderTodo();
+document.getElementById("inicios").classList.add("ativa")
+
+}
+
+function abrirBandas(){
+    esconderTodo();
+    document.getElementById("bandas").classList.add("ativa")
+    
+}
+
+function abrirColaboradores(){
+    esconderTodo();
+    document.getElementById("colaboradores").classList.add("ativa")
+    
 }
 function abrirContatos(){
-
+    esconderTodo();
+    document.getElementById("contatos").classList.add("ativa")
+    
 }
 function abrirShop(){
+    esconderTodo();
+    document.getElementById("bowshop").classList.add("ativa")
     
 }
