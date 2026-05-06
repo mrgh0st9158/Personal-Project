fca34e3ff8bb162fd5aa21cb61fcb46a00601738
ImDr3ek <ggdiego581@gmail.com>
2026-05-05 22:20:07 -0300

    Jogo novo

--- Joguinhos-test/adivinhaonumero.js
+++ Joguinhos-test/adivinhaonumero.js
@@ -1,17 +1,16 @@
 
 
-let numeroAleatorio, dificultad, resposta
+let numeroAleatorio, dificultad, resposta, tentativas
 
 function criarNumeroAleatorio(){
-dificultad=Number(prompt("Nivel de Dificultad(coloca qualquer numero)"))
-numeroAlearotio =Number(Math.ceil(Math.random() * dificultad))
+dificultad=Number(prompt("Nivel de Dificuldade(coloca qualquer numero)"))
+numeroAleatorio =Number(Math.ceil(Math.random() * dificultad))
 let dificuldade=document.getElementById('dificuldade')
 if(numeroAleatorio <= 50){
     dificuldade.innerHTML ="Dificuldade Facil !ENCONTA O NUMERO ENTRE 0 e  " + dificultad
 
 }else if(numeroAleatorio<=100){
     dificuldade.innerHTML ="Dificuldade mais ou menos !ENCONTA O NUMERO ENTRE 0 e " + dificultad
-    
 }else if(numeroAleatorio<=200){
     dificuldade.innerHTML ="Dificuldade media !ENCONTA O NUMERO ENTRE 0 e " + dificultad
     
