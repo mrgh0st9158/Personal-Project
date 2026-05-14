abddb334d72aa8d57586ddc5df8ba001ed23e438
mrgh0st9158 <athosclpimenta@gmail.com>
2026-05-13 21:56:56 -0300

    falta correção de bugs

--- PortFolio/acountscript.js
+++ PortFolio/acountscript.js
@@ -1,37 +1,37 @@
 let criarNovoUsuario, novaSenha, usuario, senha 
 
 
-function esconderTodo(){
-  const tela = document.querySelectorAll(".tela");
-  tela.forEach(function(tela){
+ function esconderTodo(){
+   const tela = document.querySelectorAll(".tela");
+   tela.forEach(function(tela){
     tela.classList.remove("ativa")});
  
-}
-function novoUsuario(){
+  }
+ function novoUsuario(){
     esconderTodo();
     document.getElementById("cadastrarConta").classList.add("ativa")
-}
+  }
 
-function criarConta(){
+ function criarConta(){
 
   criarNovoUsuario = document.getElementById("usuarioNovo").innerHTML = criarNovoUsuario
   novaSenha = document.getElementById("senhaNova").innerHTML = novaSenha
 
-esconderTodo();
-document.getElementById("inicioSeccion").classList.add("ativa")
-}
+  esconderTodo();
+  document.getElementById("inicioSeccion").classList.add("ativa")
+  }
 
-function iniciaSeccion(){
+ function iniciaSeccion(){
 
   usuario = document.getElementById("usuarios").innerHTML = usuario
   senha = document.getElementById("senhas").innerHTML = senha
 
-  if(usuario != criarNovoUsuario || senha != novaSenha){
-
-    document.getElementById("errado").innerHTML = "Nome ou Senha errado!"
+  if(usuario == criarNovoUsuario && senha == novaSenha){
+    
+    window.location = "Index.html"
   }
   else{
-    window.location = "Index.html"
+    document.getElementById("errado").innerHTML = "Nome ou Senha errado!"
   }
 
 }
